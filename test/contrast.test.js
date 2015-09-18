4e3d2335237407824c04e9cf784a16e91f38870e
David Pett <me@davidpett.com>
2015-09-18 15:22:41 -0500

    fixed failing test

--- test/contrast.test.js
+++ test/contrast.test.js
@@ -43,7 +43,7 @@ describe('contrast', function() {
             expect(contrast.hex(whiteHex, whiteHex)).to.eql(1);
         });
         it('opposite luminance', function() {
-            expect(contrast.hex(blackHex, blackHex)).to.eql(21);
+            expect(contrast.hex(whiteHex, blackHex)).to.eql(21);
         });
     });
 });
