d3f3af46e04819b01841b63ca0bbadedeaf9055b
Tom MacWright <tom@macwright.org>
2016-11-30 13:35:41 -0500

    Use JSDoc. Fixes #5

--- index.js
+++ index.js
@@ -1,46 +1,49 @@
-var luminance = require('relative-luminance');
+var relativeLuminance = require('relative-luminance');
 var hexRgb = require('hex-rgb');
 
 // http://www.w3.org/TR/WCAG20/#contrast-ratiodef
 
-module.exports.luminance = fromLum;
-module.exports.rgb = fromRGB;
-module.exports.hex = fromHex;
+module.exports.luminance = luminance;
+module.exports.rgb = rgb;
+module.exports.hex = hex;
 module.exports.score = score;
 
-/*
+/**
+ * Get the contrast ratio between two relative luminance values
  * @param {number} a luminance value
  * @param {number} b luminance value
  * @returns {number} contrast ratio
  */
-function fromLum(a, b) {
+function luminance(a, b) {
     var l1 = Math.max(a, b),
         l2 = Math.min(a, b);
     return (l1 + 0.05) / (l2 + 0.05);
 }
 
-/*
- * @param {array} a rgb value
- * @param {array} b rgb value
+/**
+ * Get a score for the contrast between two colors as rgb triplets
+ * @param {array} a
+ * @param {array} b
  * @returns {number} contrast ratio
  */
-function fromRGB(a, b) {
-    return fromLum(luminance(a), luminance(b));
+function rgb(a, b) {
+    return luminance(relativeLuminance(a), relativeLuminance(b));
 }
 
-/*
+/**
+ * Get a score for the contrast between two colors as hex strings
  * @param {string} a hex value
  * @param {string} b hex value
  * @returns {number} contrast ratio
  */
-function fromHex(a, b) {
-    return fromRGB(hexRgb(a), hexRgb(b));
+function hex(a, b) {
+    return rgb(hexRgb(a), hexRgb(b));
 }
 
-/*
- * @param {array} a rgb value
- * @param {array} b rgb value
- * @returns {number} contrast ratio
+/**
+ * Get a textual score from a numeric contrast value
+ * @param {number} contrast
+ * @returns {string} score
  */
 function score(contrast) {
     return (contrast >= 7) ? 'AAA' : (contrast >= 4.5) ? 'AA' : '';
