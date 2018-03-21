95a16e96a9b3f422c1fd347760aa4691f9229ca2
Tom MacWright <tom@macwright.org>
2018-03-21 16:10:45 -0700

    feat: Internalize hexRgb dependency, making this and its dependents all es6-safe

--- index.mjs
+++ index.mjs
@@ -1,5 +1,34 @@
 import relativeLuminance from "relative-luminance";
-import hexRgb from "hex-rgb";
+
+const hexChars = "a-f\\d";
+const match3or4Hex = `#?[${hexChars}]{3}[${hexChars}]?`;
+const match6or8Hex = `#?[${hexChars}]{6}([${hexChars}]{2})?`;
+
+function hexRgb(hex) {
+  hex = hex.replace(/^#/, "");
+  let alpha = 255;
+
+  if (hex.length === 8) {
+    alpha = parseInt(hex.slice(6, 8), 16);
+    hex = hex.substring(0, 6);
+  }
+
+  if (hex.length === 4) {
+    alpha = parseInt(hex.slice(3, 4).repeat(2), 16);
+    hex = hex.substring(0, 3);
+  }
+
+  if (hex.length === 3) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+  }
+
+  const num = parseInt(hex, 16);
+  const red = num >> 16;
+  const green = (num >> 8) & 255;
+  const blue = num & 255;
+
+  return [red, green, blue, alpha];
+}
 
 // http://www.w3.org/TR/WCAG20/#contrast-ratiodef
 
